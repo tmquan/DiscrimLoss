a4323e176e4ca1f2d5223a06aaf259c1b6ec86ca
tmquan <quantm@unist.ac.kr>
2018-04-10 18:51:19 -0400

    Experiment works for CVPPP data

--- Utility.py
+++ Utility.py
@@ -37,30 +37,144 @@ import tensorflow as tf
 # Tensorlayer
 from tensorlayer.cost import binary_cross_entropy, absolute_difference_error, dice_coe
 
-# Sklearn
-from sklearn.metrics.cluster import adjusted_rand_score
+# Augmentation
+import Augmentor
+
 ###############################################################################
+NB_FILTERS = 32
+###############################################################################
+def time_seed ():
+	seed = None
+	while seed == None:
+		cur_time = time.time ()
+		seed = int ((cur_time - int (cur_time)) * 1000000)
+		return seed
 
 
-class CVPPPImageDataFlow(RNGDataFlow):
-	def __init__(self, imageDir, labelDir, size, dtype='float32', isTrain=False, isValid=False, isTest=False):
-		self.dtype      = dtype
-		self.imageDir   = imageDir
-		self.labelDir   = labelDir
-		self._size      = size
-		self.isTrain    = isTrain
-		self.isValid    = isValid
+###############################################################################
+def INReLU(x, name=None):
+	x = InstanceNorm('inorm', x)
+	return tf.nn.relu(x, name=name)
 
-		images = natsorted (glob.glob(self.imageDir + '/*rgb.png'))
-		labels = natsorted (glob.glob(self.labelDir + '/*label.png'))
-		self.images = []
-		self.labels = []
-		self.data_seed = time_seed ()
-		self.data_rand = np.random.RandomState(self.data_seed)
-		self.rng = np.random.RandomState(999)
-		for i in range (len (images)):
-			image = images[i]
-			label = labels[i]
-		self.images.append (skimage.io.imread (image))
-		self.labels.append (skimage.io.imread (label))
-		
+
+def INLReLU(x, name=None):
+	x = InstanceNorm('inorm', x)
+	return tf.nn.leaky_relu(x, name=name)
+	
+def BNLReLU(x, name=None):
+	x = BatchNorm('bn', x)
+	return tf.nn.leaky_relu(x, name=name)
+
+###############################################################################
+def toMaxLabels(label, factor=320):
+	result = tf.cast(label, tf.float32)
+	status = tf.equal(result, -1.0*tf.ones_like(result))
+	result = tf.where(status, tf.zeros_like(result), result, name='removedBackground') # From -1 to 0
+	result = result * factor # From 0~1 to 0~MAXLABEL
+	result = tf.round(result)
+	return tf.cast(result, tf.int32)
+###############################################################################
+# Utility function for scaling 
+def tf_2tanh(x, maxVal = 255.0, name='ToRangeTanh'):
+	with tf.variable_scope(name):
+		return (x / maxVal - 0.5) * 2.0
+###############################################################################
+def tf_2imag(x, maxVal = 255.0, name='ToRangeImag'):
+	with tf.variable_scope(name):
+		return (x / 2.0 + 0.5) * maxVal
+
+# Utility function for scaling 
+def np_2tanh(x, maxVal = 255.0, name='ToRangeTanh'):
+	return (x / maxVal - 0.5) * 2.0
+###############################################################################
+def np_2imag(x, maxVal = 255.0, name='ToRangeImag'):
+	return (x / 2.0 + 0.5) * maxVal
+
+
+###############################################################################
+# FusionNet
+@layer_register(log_shape=True)
+def residual(x, chan, first=False):
+	with argscope([Conv2D], nl=INLReLU, stride=1, kernel_shape=3):
+		input = x
+		return (LinearWrap(x)
+				.Conv2D('conv1', chan, padding='SAME', dilation_rate=1)
+				.Conv2D('conv2', chan, padding='SAME', dilation_rate=2)
+				.Conv2D('conv4', chan, padding='SAME', dilation_rate=4)             
+				.Conv2D('conv5', chan, padding='SAME', dilation_rate=8)
+				.Conv2D('conv0', chan, padding='SAME', nl=tf.identity)
+				.InstanceNorm('inorm')()) + input
+
+###############################################################################
+@layer_register(log_shape=True)
+def Subpix2D(inputs, chan, scale=2, stride=1):
+	with argscope([Conv2D], nl=INLReLU, stride=stride, kernel_shape=3):
+		results = Conv2D('conv0', inputs, chan* scale**2, padding='SAME')
+		old_shape = inputs.get_shape().as_list()
+		# results = tf.reshape(results, [-1, chan, old_shape[2]*scale, old_shape[3]*scale])
+		# results = tf.reshape(results, [-1, old_shape[1]*scale, old_shape[2]*scale, chan])
+		if scale>1:
+			results = tf.depth_to_space(results, scale, name='depth2space', data_format='NHWC')
+		return results
+
+###############################################################################
+@layer_register(log_shape=True)
+def residual_enc(x, chan, first=False):
+	with argscope([Conv2D, Deconv2D], nl=INLReLU, stride=1, kernel_shape=3):
+		x = (LinearWrap(x)
+			# .Dropout('drop', 0.75)
+			.Conv2D('conv_i', chan, stride=2) 
+			.residual('res_', chan, first=True)
+			.Conv2D('conv_o', chan, stride=1) 
+			())
+		return x
+
+###############################################################################
+@layer_register(log_shape=True)
+def residual_dec(x, chan, first=False):
+	with argscope([Conv2D, Deconv2D], nl=INLReLU, stride=1, kernel_shape=3):
+				
+		x = (LinearWrap(x)
+			.Subpix2D('deconv_i', chan, scale=1) 
+			.residual('res2_', chan, first=True)
+			.Subpix2D('deconv_o', chan, scale=2) 
+			# .Dropout('drop', 0.75)
+			())
+		return x
+
+###############################################################################
+@auto_reuse_variable_scope
+def arch_generator(img, last_dim=1):
+	assert img is not None
+	with argscope([Conv2D, Deconv2D], nl=INLReLU, kernel_shape=3, stride=2, padding='SAME'):
+		e0 = residual_enc('e0', img, NB_FILTERS*1)
+		e1 = residual_enc('e1',  e0, NB_FILTERS*2)
+		e2 = residual_enc('e2',  e1, NB_FILTERS*4)
+
+		e3 = residual_enc('e3',  e2, NB_FILTERS*8)
+		# e3 = Dropout('dr', e3, 0.5)
+
+		d3 = residual_dec('d3',    e3, NB_FILTERS*4)
+		d2 = residual_dec('d2', d3+e2, NB_FILTERS*2)
+		d1 = residual_dec('d1', d2+e1, NB_FILTERS*1)
+		d0 = residual_dec('d0', d1+e0, NB_FILTERS*1) 
+		dd =  (LinearWrap(d0)
+				.Conv2D('convlast', last_dim, kernel_shape=3, stride=1, padding='SAME', nl=tf.tanh, use_bias=True) ())
+		return dd, d0
+
+@auto_reuse_variable_scope
+def arch_discriminator(img):
+	assert img is not None
+	with argscope([Conv2D, Deconv2D], nl=INLReLU, kernel_shape=3, stride=2, padding='SAME'):
+		img = Conv2D('conv0', img, NB_FILTERS, nl=tf.nn.leaky_relu)
+		e0 = residual_enc('e0', img, NB_FILTERS*1)
+		# e0 = Dropout('dr', e0, 0.5)
+		e1 = residual_enc('e1',  e0, NB_FILTERS*2)
+		e2 = residual_enc('e2',  e1, NB_FILTERS*4)
+
+		e3 = residual_enc('e3',  e2, NB_FILTERS*8)
+
+		ret = Conv2D('convlast', e3, 1, stride=1, padding='SAME', nl=tf.identity, use_bias=True)
+		return ret
+
+
